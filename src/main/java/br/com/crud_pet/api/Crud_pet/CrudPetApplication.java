1161952470413f273ed093653f1555c4822cd0a9
Thues021 <mateusoliver2109@hotmail.com>
2024-02-27 13:48:06 -0300

    first_commit

---
+++ src/main/java/br/com/crud_pet/api/Crud_pet/CrudPetApplication.java
@@ -0,0 +1,13 @@
+package br.com.crud_pet.api.Crud_pet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudPetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudPetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/crud_pet/api/Crud_pet/CrudPetApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.crud_pet.api.Crud_pet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudPetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
