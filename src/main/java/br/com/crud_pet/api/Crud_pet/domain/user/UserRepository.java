40c6a5d7b155cc0b0bf74ab0cf9295b22f6435fd
Matheus Oliveira <mateusoliver2109@hotmail.com>
2024-03-12 20:18:48 -0300

    created_user_controller

---
+++ src/main/java/br/com/crud_pet/api/Crud_pet/domain/user/UserRepository.java
@@ -0,0 +1,9 @@
+package br.com.crud_pet.api.Crud_pet.domain.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+    UserDetails findByUsername(String login);
+}
---
+++ src/main/java/br/com/crud_pet/api/Crud_pet/infra/service/AuthorizationService.java
@@ -0,0 +1,18 @@
+package br.com.crud_pet.api.Crud_pet.infra.service;
+
+import br.com.crud_pet.api.Crud_pet.domain.user.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+public class AuthorizationService implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return repository.findByUsername(username);
+    }
+}
